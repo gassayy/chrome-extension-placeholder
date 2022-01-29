568f1011b7da310d66448e976377a4cba09c1c50
c3v6a2vy <c3v6a2vy@protonmail.com>
2022-01-28 23:04:32 -0800

    changed ntf link from div to button

--- src/extension/Content/AgumentedElements/NFTButton.ts
+++ src/extension/Content/AgumentedElements/NFTButton.ts
@@ -1,4 +1,4 @@
-let nftButton = document.createElement('div')
+let nftButton = document.createElement('button')
 nftButton.setAttribute('class', 'bar-item-wrappe')
 let nftLink = document.createElement('a')
 nftLink.setAttribute('href', 'https://fancy-limit-9973.on.fleek.co/')
