63708c0fd3709982e17aa2272c9cce59cb6e1a87
tieyingjing <terry.ty.jing@gmail.com>
2022-01-31 10:32:39 -0500

    signup page initial commit

--- src/extension/Pages/SignUp/Components/UserInfoForm.tsx
+++ src/extension/Pages/SignUp/Components/UserInfoForm.tsx
@@ -1,26 +1,75 @@
-import { useForm } from 'react-hook-form'
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-type UserInfo = {
-  username: string
-  email: string
-}
+const theme = createTheme();
 
 export default function UserInfoForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<UserInfo>()
-
-  const onSubmit = handleSubmit((data) => {
-    console.log(data)
-  })
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" {...register('username')} />
-      <input type="email" {...register('email')} />
-      <input type="submit" />
-    </form>
-  )
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        {/* <CssBaseline /> */}
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <AccountCircleIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="username"
+              label="User name"
+              type="username"
+              id="username"
+              autoComplete="username"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
